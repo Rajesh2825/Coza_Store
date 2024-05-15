b8bd1092f5f0f2c47cbdfbb7f97123da1e5a4203
Rajesh2825 <114496189+Rajesh2825@users.noreply.github.com>
2024-05-15 17:11:27 +0530

    Update settings.py

--- coza_store/settings.py
+++ coza_store/settings.py
@@ -19,8 +19,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'rajeshradadiya99@gmail.com'
-EMAIL_HOST_PASSWORD = 'ubugqhzyvuyoloya'
+EMAIL_HOST_USER = 'your_email@gmail.com'
+EMAIL_HOST_PASSWORD = 'Password'
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
